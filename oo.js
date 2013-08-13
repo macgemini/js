f8b48b6561f8ebd078b1761144e7f3b6ba7338a3
macgemini <gememacks@gmail.com>
2013-08-13 15:05:38 +0200

    toString added

--- oo.js
+++ oo.js
@@ -44,6 +44,75 @@ Grid.prototype.isInside = function(point) {
 };
 
 Grid.prototype.moveValue = function(from, to) {
-    this.setValueAt(to, this.valueAt(from);
+    this.setValueAt(to, this.valueAt(from));
     this.setValueAt(from , undefined);
+};
+
+Grid.prototype.each = function(action) {
+  for (var y = 0; y < this.height; y++) {
+    for (var x = 0; x < this.width; x++) {
+      var point = new Point(x, y);
+      action(point, this.valueAt(point));
+    }
+  }
+};
+
+var directions = new Dictionary(
+  {"n":  new Point( 0, -1),
+   "ne": new Point( 1, -1),
+   "e":  new Point( 1,  0),
+   "se": new Point( 1,  1),
+   "s":  new Point( 0,  1),
+   "sw": new Point(-1,  1),
+   "w":  new Point(-1,  0),
+   "nw": new Point(-1, -1)});
+
+function StupidBug() {};
+StupidBug.prototype.act = function(surroundings) {
+  return {type: "move", direction: "s"};
+};
+
+
+var wall = {};
+
+function Terrarium(plan) {
+  var grid = new Grid(plan[0].length, plan.length);
+  for (var y = 0; y < plan.length; y++) {
+    var line = plan[y];
+    for (var x = 0; x < line.length; x++) {
+      grid.setValueAt(new Point(x, y),
+		      elementFromCharacter(line.charAt(x)));
+    }
+  }
+  this.grid = grid;
 }
+
+function elementFromCharacter(character) {
+  if (character == " ")
+    return undefined;
+  else if (character == "#")
+    return wall;
+  else if (character == "o")
+    return new StupidBug();
+}
+
+wall.character = "#";
+StupidBug.prototype.character = "o";
+
+function characterFromElement(element) {
+  if (element == undefined)
+    return " ";
+  else
+    return element.character;
+}
+
+Terarium.prototype.toString = function() {
+    var result = [];
+    var endOfLine = this.grid.width - 1;
+    this.grid.each(function(point,value) {
+	result.push(characterFromElement(value));
+	if (point.x == endOfLine)
+	    result.push("\n");
+    });
+    return result.join("");
+};
